f6177f141d86cae609bcb62c6a64b707f0123b63
Marina De Castro <132936941+cyn1calmars@users.noreply.github.com>
2024-12-03 18:07:21 -0800

    Update Login.js

--- Login.js
+++ Login.js
@@ -2,10 +2,6 @@
 function printError(elemId, msg){
     document.getElementById(elemId).innerHTML = msg;
 }
-function printvalid(elemId, msg){
-    document.getElementById(elemId).innerHTML = msg;
-}
-
 //this function will ensure that the form inputs are valid
 function validateForm(){
     //these are the inputs from the html text/input elements
@@ -19,10 +15,12 @@ var pass_valid = true;
 printError("user_error", "");
 printError("pass_error", "");
 
+//Input is empty for username textbox
 if (user == ""){
     printError("user_error", "Please enter your username");
     user_valid = false;
 }
+// Welcome User is shown below if the textbox isn't empty
 else{
     user_result.innerHTML= "Welcome " + "'" + user + "'";
     user_valid = false;
@@ -33,11 +31,13 @@ if (psw == ""){
     printError("pass_error", "Please enter your password");
     pass_valid = false;
 }
+// Shows password below if the textbox isn't empty
 else{
     pass_result.innerHTML= "Password entered: " + "'" + psw + "'" ;
     user_valid = false;
 }
 
+    //If any of the textboxes are empty
 if(!user_valid || !pass_valid){
     return false;
 }
